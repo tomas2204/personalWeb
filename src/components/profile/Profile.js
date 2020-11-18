cbc5a3098a4ee05d8dc9c943e0e8cc6bc8b06157
Tomas Maldonado <tomasmaldonado297@gmail.com>
2020-11-18 20:52:51 -0300

    mejorando la interfaz grafica

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,37 +1,49 @@
 import React from 'react';
 import './Profile.css'
 import logo from './miLogo.png' 
+
 export default function Profile(){
 
     return (
         <div className="root">
             <div className="upBar">
                 <div className="links">
-                    <a className="titleName" href="/">Tomás Maldonado</a>
+                    <a href="/">Inicio</a>
                     <a href="/curriculum">Curriculum</a>
                     <a href="/info">Sobre mí</a>
                     <a href="/contacto">Contacto</a>
                 </div>
             </div>
+            <div className="presentation">
+                <span className="saludo">
+                    <h2 className="titleName">Tomás Agustin</h2>
+                    <h2 className="titleName">Maldonado Sarmiento</h2>
+                </span>
+                <span className="identity">
+                    <img id="imgInicio" className="imgInicio" src={logo} alt="nothing"/>
+                    <span className="conoceme">
+                        <h4 >Este es mi Hola mundo</h4>
+                        <h4 >¿Te gustaria conocerme un poco ?</h4>
+                    </span>
+                </span>
+            </div>
             <div className="information">
-                    <img className="imgInicio" src={logo} alt="nothing"/>
                 <div className="tips">
-                    <div className="tip">
-                        {/* <p>Nombre</p> */}
-                        <span class="tiny material-icons" >arrow_right_alt</span>
-                        <p>Tomás Agustín Maldonado Sarmiento</p>
-                    </div>
-                    <div className="tip">
-                        {/* <p>Titulo</p> */}
+                    <span className="tip">
+                        <p>Titulo</p>
                         <span class="tiny material-icons" >arrow_right_alt</span>
                         <p>Desarrollador web full stack</p>
-                    </div>
-                    <div className="tip">
+                    </span>
+                    <span className="tip">
                         <span class="tiny material-icons" >arrow_right_alt</span>
-                    </div>
-                    <div className="tip">
+                        <p>Desarrollador web full stack</p>
+                    </span>
+                    <span className="tip">
                         <span class="tiny material-icons" >arrow_right_alt</span>
-                    </div>
+                    </span>
+                    <span className="tip">
+                        <span class="tiny material-icons" >arrow_right_alt</span>
+                    </span>
                 </div>
             </div>
         </div>
